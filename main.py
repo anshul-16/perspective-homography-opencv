8271d5d8524be9de8714408b243f991eaf1e3297
Anshul Sachdev <anshul.sachdev2016@vitstudent.ac.in>
2019-05-12 01:49:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+# Importing the dependencies
+import cv2
+import numpy as np
+
+# Defining variables to store coordinates where the second image has to be placed
+positions=[] 
+positions2=[]
+count=0
+
+# Mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global positions,count
+    # If event is Left Button Click then store the coordinate in the lists, positions and positions2
+    if event == cv2.EVENT_LBUTTONUP:
+        cv2.circle(building,(x,y),2,(255,0,0),-1)
+        positions.append([x,y])
+        if(count!=3):
+            positions2.append([x,y])
+        elif(count==3):
+            positions2.insert(2,[x,y])
+        count+=1
+        
+# Reading the two images and storing it in variables img and dp
+building = cv2.imread('building.jpeg')
+dp = cv2.imread('dp.jpg')
+
+# Defing a window named 'image'
+cv2.namedWindow('image')
+
+cv2.setMouseCallback('image',draw_circle)
+
+while(True):
+    cv2.imshow('image',building)
+    k = cv2.waitKey(20) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+
+height, width = building.shape[:2]
+h1,w1 = dp.shape[:2]
+
+pts1=np.float32([[0,0],[w1,0],[0,h1],[w1,h1]])
+pts2=np.float32(positions)
+
+
+h, mask = cv2.findHomography(pts1, pts2, cv2.RANSAC,5.0)
+
+height, width, channels = building.shape
+im1Reg = cv2.warpPerspective(dp, h, (width, height))
+cv2.imwrite('third.png',im1Reg)
+
+mask2 = np.zeros(building.shape, dtype=np.uint8)
+cv2.imwrite('fourth.png',mask2)
+
+roi_corners2 = np.int32(positions2)
+
+channel_count2 = building.shape[2]  
+ignore_mask_color2 = (255,)*channel_count2
+
+cv2.fillConvexPoly(mask2, roi_corners2, ignore_mask_color2)
+
+mask2 = cv2.bitwise_not(mask2)
+masked_image2 = cv2.bitwise_and(building, mask2)
+
+#Using Bitwise or to merge the two images
+final = cv2.bitwise_or(im1Reg, masked_image2)
+cv2.imshow('final.png',final)
+
+
+
